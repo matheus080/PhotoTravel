53ccccde9d9bd1a766b5fe03f6ac8e26a67770b6
Matheus Pavanetti <matheus.pavanetti@daruma.com.br>
2017-09-05 10:38:47 -0300

    Inicializando
    Primeiro Commit - Inicializando

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var working = false;
+$('.login').on('submit', function(e) {
+  e.preventDefault();
+  if (working) return;
+  working = true;
+  var $this = $(this),
+    $state = $this.find('button > .state');
+  $this.addClass('loading');
+  $state.html('Autenticando');
+  setTimeout(function() {
+    $this.addClass('ok');
+    $state.html('Bem Vindo de volta!');
+    setTimeout(function() {
+      $state.html('Log in');
+      $this.removeClass('ok loading');
+      working = false;
+    }, 4000);
+  }, 3000);
+});
+
